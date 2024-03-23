74d61d579a3721cbf981199ef16b939c10708feb
Trm200202 <ruslan231102@gmail.com>
2024-03-23 12:00:11 +0500

    salom

--- config/settings.py
+++ config/settings.py
@@ -122,10 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
+STATICFILES_DIRS =  [BASE_DIR / "staticfiles"]
 
 
 MEDIA_URL = "media/"
